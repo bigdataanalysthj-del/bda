a57c092bfd7731881468e2e8e62b39d94d0bacf1
bigdataanalysthj-del <bigdataanalyst.hj@gmail.com>
2025-08-05 11:44:06 +0900

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,53 @@
+# Day 1
+import streamlit as st
+st.write('Hello world!')
+
+
+# Day 2
+
+# 엡 헤더 문구
+st.header('st.button')
+
+# 버튼 출력 후 동작 설정
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
+
+
+# Dat 5
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.header('st.write')
+
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
